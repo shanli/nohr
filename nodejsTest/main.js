707c1f1a15e74dc08fee5c2ae2c466ae024ed6fa
山莉 <shanli1115@gmail.com>
2012-12-20 13:20:51 +0800

    Revert "add nohr"
    This reverts commit 7ef415aeb54552a4506060c6eae8afa87832e2c6.

--- nodejsTest/main.js
+++
@@ -1,7 +0,0 @@
-var runner = require('../../node_modules/qunit');
-runner.run({
-    code : '/home/gc/test-n2p/nodejsTest/src/mock.js',
-    tests: '/home/gc/test-n2p/nodejsTest/tests/test.js'
-}, function(err, report) {
-    console.dir(report);
-});
--- static/js/init.js
+++
@@ -1,5 +0,0 @@
-define(function ( require, exports, modules ) {
-    var IndexRouter = require('./router/indexRouter'),
-        indexRouter = new IndexRouter;
-    Backbone.history.start();
-});
